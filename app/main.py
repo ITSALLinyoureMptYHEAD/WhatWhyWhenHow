5ce74afec2e3282ce676fc3bae025fc6f54cac54
Dominika C <dominikacich303@gmail.com>
2026-02-20 11:51:00 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,34 +9,35 @@ def main():
         builtins = {"echo", "exit", "type"}
         if command == "exit":
             break
-        if args[1] in ("echo", "exit", "type"):
-            print(f"{args[1]} is a shell builtin")
         else:
             print(f"{args[1]}: not found")
         elif:
-            print(f"{command}: command not found")
+        print(f"{command}: command not found")
         elif command.startswith("echo "):
-            args = command.split("echo", " ", 1)
-            print(args[1])
+        args = command.split("echo", " ", 1)
+        print(args[1])
         elif command.startswith("echo"):
-            args = command.split("echo", 1)
-            print(args[1])
+        args = command.split("echo", 1)
+        print(args[1])
         elif command.startswith("type "):
-            args = command.split(" ", 1)
-            command_name = args[1]
+        args = command.split(" ", 1)
+        command_name = args[1]
         elif command.startswith("type"):
-            args = command.split("type", 1)
-            command_name = args[1]
-        if not builtin:
-        path = os.environ.get("PATH", "")
-        path_separator = os.pathsep
-        os.access(path, os.X_OK)
-        for directory in path.split(path_separator):
-            full_path = os.path.join(directory, command_name)
+        args = command.split("type", 1)
+        command_name = args[1]
+        if args[1] in ("echo", "exit", "type"):
+            print(f"{args[1]} is a shell builtin")
+        else:
+            path = os.environ.get("PATH", "")
+            path_separator = os.pathsep
+            os.access(path, os.X_OK)
+            for directory in path.split(path_separator):
+                full_path = os.path.join(directory, command_name)
             if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
                 print(f"{command_name} is {full_path}")
-                return
-        if not os.path.isdir(directory):
+                found = True
+                break
+            if not os.path.isdir(directory):
             OSError:
             continue
 
