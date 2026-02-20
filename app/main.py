15bbec518d264f1dcf2917a57442464d312e2250
Dominika C <dominikacich303@gmail.com>
2026-02-20 12:20:47 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,19 +7,20 @@ def main():
         sys.stdout.write("$ ")
         command = input()
         builtins = {"echo", "exit", "type"}
-            if command == "exit":
-                break
-            elif command.startswith("echo ") or command.startswith("echo"):
-                args = command.split("echo ", 1) or args = command.split("echo", 1)
-                print(args[1])
-            elif command.startswith("type ") or command.startswith("type"):
+        if command == "exit":
+            break
+        elif command.startswith("echo "):
+            args = command.split("echo ", 1)
+            print(args[1])
+        elif command == "echo":
+            print("")
+        elif command.startswith("type ") or command.startswith("type"):
                 args = command.split("type ", 1) or args = command.split("type", 1)
                 command_name = args[1]
                 if args[1] in ("echo", "exit", "type"):
                     print(f"{args[1]} is a shell builtin")
-                    break
-                else:
-                    if not builtins:
+                    continue
+                elif not builtins:
                         found = False
                         path = os.environ.get("PATH", "")
                         path_separator = os.pathsep
@@ -29,16 +30,16 @@ def main():
                             if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
                                 print(f"{command_name} is {full_path}")
                                 found = True
-                                break
-                        if not os.path.isdir(directory):
-                            print(f"{args[1]}: not found")
-                            break
-                        elif OSError:
-                            print(f"OSError, FIX ME!")
-                            break
-                        else:
+                                continue
+                            if not os.path.isdir(directory):
+                                print(f"{args[1]}: not found")
+                                continue
+                            elif OSError:
+                                print(f"OSError, FIX ME!")
+                                continue
+                else:
                             print(f"{command}: command not found")
-                            break
+                            continue
 
 if __name__ == "__main__":
     main()
