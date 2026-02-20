ffe4376cc8ae51e9bbb28bef715e68ad02a07001
Dominika C <dominikacich303@gmail.com>
2026-02-20 12:02:38 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,39 +7,40 @@ def main():
         sys.stdout.write("$ ")
         command = input()
         builtins = {"echo", "exit", "type"}
-        if command == "exit":
-            break
-        else:
-            print(f"{args[1]}: not found")
-        elif:
-        print(f"{command}: command not found")
-        elif command.startswith("echo "):
-        args = command.split("echo", " ", 1)
-        print(args[1])
-        elif command.startswith("echo"):
-        args = command.split("echo", 1)
-        print(args[1])
-        elif command.startswith("type "):
-        args = command.split(" ", 1)
-        command_name = args[1]
-        elif command.startswith("type"):
-        args = command.split("type", 1)
-        command_name = args[1]
-        if args[1] in ("echo", "exit", "type"):
-            print(f"{args[1]} is a shell builtin")
-        else:
-            path = os.environ.get("PATH", "")
-            path_separator = os.pathsep
-            os.access(path, os.X_OK)
-            for directory in path.split(path_separator):
-                full_path = os.path.join(directory, command_name)
-            if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-                print(f"{command_name} is {full_path}")
-                found = True
+            if command == "exit":
                 break
-            if not os.path.isdir(directory):
-            OSError:
-            continue
+            elif command.startswith("echo "):
+                args = command.split("echo", " ", 1)
+                print(args[1])
+            or command.startswith("echo"):
+                args = command.split("echo", 1)
+                print(args[1])
+            elif command.startswith("type "):
+                args = command.split(" ", 1)
+                command_name = args[1]
+            or command.startswith("type"):
+                args = command.split("type", 1)
+                command_name = args[1]
+                if args[1] in ("echo", "exit", "type"):
+                    print(f"{args[1]} is a shell builtin")
+                    break
+                else:
+                path = os.environ.get("PATH", "")
+                path_separator = os.pathsep
+                os.access(path, os.X_OK)
+                for directory in path.split(path_separator):
+                    full_path = os.path.join(directory, command_name)
+                    if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                        print(f"{command_name} is {full_path}")
+                        found = True
+                        break
+                    if not os.path.isdir(directory):
+                        OSError:
+                        continue
+                elif:
+                    print(f"{args[1]}: not found")
+                else:
+                    print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
