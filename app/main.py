1b66b3b1fd179f3e6ae0b148b446bcadd0c33ffb
Dominika C <dominikacich303@gmail.com>
2026-02-22 00:03:51 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ def main():
             if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
                 continue
             args = command.split(f"{command} ", 1, 2)
-            print(f"{command}" + 1 + 2)
+            print(f"{command}" + [1] + [2])
 
         else:
             print(f"{command}: command not found")
