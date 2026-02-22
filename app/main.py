e2dfd7224d2d084880e28945c91671ed05afcb4d
Dominika C <dominikacich303@gmail.com>
2026-02-23 00:43:08 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ def main():
             if args[1] in ("echo", "exit", "type", "pwd", "cd"):
                 print(f"{args[1]} is a shell builtin")
                 continue
-            elif command_name not in builtins:
+            elif command.split()[0] not in builtins:
                 path = os.environ.get("PATH", "")
                 path_separator = os.pathsep
                 for directory in path.split(path_separator):
@@ -36,7 +36,7 @@ def main():
                     except OSError:
                         break
 
-        elif command not in builtins:
+        elif command.split()[0] not in builtins:
             found = False
             path = os.environ.get("PATH", "")
             path_separator = os.pathsep
