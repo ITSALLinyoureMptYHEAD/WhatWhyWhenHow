c5fef23f00271d41b0dc2477cf6857282399ea88
Dominika C <dominikacich303@gmail.com>
2026-02-23 00:09:23 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -103,6 +103,7 @@ def main():
                 print(f"cd: {destination}: No such file or directory")
             except NotADirectoryError:
                 print(f"cd: {destination}: Not a directory")
+            return
 
 
 if __name__ == "__main__":
