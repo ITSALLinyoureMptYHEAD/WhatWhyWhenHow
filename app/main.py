216517822de423e3db87d7184dbeafc5b3aa50ef
Dominika C <dominikacich303@gmail.com>
2026-02-22 22:26:47 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,90 +1,47 @@
 import sys
 import os
+import subprocess
+
+BUILTINS = {
+    "exit": lambda: sys.exit(),
+    "echo": lambda *args: sys.stdout.write(" ".join(args) + "\n"),
+    "type": lambda *args: _get_type(args),
+    "pwd": lambda: sys.stdout.write(os.getcwd() + "\n"),
+}
+
+
+def _find_exec_path(file):
+    PATH = os.environ.get("PATH")
+    for directory in PATH.split(":"):
+        file_path = os.path.join(directory, file)
+        if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
+            return file_path
+        return None
+
+
+def _get_type(commands):
+    for command in commands:
+        if command in BUILTINS:
+            sys.stdout.write(f"{command} is a shell builtin\n")
+        elif file_path := _find_exec_path(command):
+            sys.stdout.write(f"{command} is {file_path}\n")
+        else:
+            sys.stdout.write(f"{command}: not found\n")
 
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        BUILTINS = {
-            "exit": lambda: sys.exit(),
-            "echo": lambda *args: sys.stdout.write(" ".join(args) + "\n"),
-            "type": lambda *args: _get_type(args),
-            "pwd": lambda: sys.stdout.write(os.getcwd() + "\n"),
-        }
-        if command == "exit":
-            break
-        elif command.startswith("echo "):
-            args = command.split("echo ", 1)
-            print(args[1])
-        elif command == "echo":
-            print("")
-        elif command.startswith("type"):
-            found = False
-            args = command.split("type ", 1)
-            command_name = args[1]
-            if args[1] in ("echo", "exit", "type", "pwd"):
-                print(f"{args[1]} is a shell builtin")
-                continue
-            elif command_name not in BUILTINS:
-                path = os.environ.get("PATH", "")
-                path_separator = os.pathsep
-                for directory in path.split(path_separator):
-                    full_path = os.path.join(directory, command_name)
-                    if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-                        print(f"{command_name} is {full_path}")
-                        found = True
-                        break
-                if not found:
-                    try:
-                        print(f"{command_name}: not found")
-                    except OSError:
-                        break
+        cmd = input()
+        first = cmd.split(" ")[0]
+        rest = cmd.split(" ")[1:]
 
-        elif command not in BUILTINS:
-            found = False
-            path = os.environ.get("PATH", "")
-            path_separator = os.pathsep
-            parts = command.split()
-            command_name = parts[0]
-            for directory in path.split(path_separator):
-                full_path = os.path.join(directory, command_name)
-                if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-                    found = True
-                    break
-            if found:
-                pid = os.fork()
-                if pid == 0:
-                    os.execvp(command_name, parts)
-                else:
-                    os.waitpid(pid, 0)
-            if not found:
-                try:
-                    print(f"{command_name}: not found")
-                except OSError:
-                    break
-
-        elif command.startswith("pwd "):
-            found = False
-            args = command.split("type ", 1)
-            print_working_directory = args[1]
-            path = os.environ.get("PATH", "")
-            path_separator = os.pathsep
-            for directory in path.split(path_separator):
-                full_path = os.path.join(directory, command_name)
-                if os.path.isfile(full_path):
-                    found = True
-                    break
-            if found:
-                print({full_path})
-            if not found:
-                try:
-                    print(f"{print_working_directory}: path not found")
-                except OSError:
-                    break
-
+        if first in BUILTINS:
+            BUILTINS[first](*rest)
+        elif _find_exec_path(first):
+            subprocess.run(cmd.split(" "), check=True)
         else:
-            print(f"{command}: command not found")
+            sys.stdout.write(f"{cmd}: command not found\n")
 
 
 if __name__ == "__main__":
