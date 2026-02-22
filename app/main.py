a699f65199bc82456298d3732f57fd08d0564e6c
Dominika C <dominikacich303@gmail.com>
2026-02-23 00:36:53 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -69,7 +69,7 @@ def main():
         elif command == "cd" or command.startswith("cd"):
             parts = command.split(" ", 1)
             # Split the command to see if there's a second part
-            if len(parts) == 1 or parts[1].strip() == "":
+                if len(parts) == 1 or parts[1].strip() == "":
                 #
                 # How .strip() works:
                 # .strip() only removes invisible "whitespace" (like if you accidentally hit the spacebar...
@@ -79,30 +79,30 @@ def main():
                 # It doesn't touch the letters in the middle.
                 #
                 # No folder specified? Go to the HOME directory!
-                destination = os.environ.get("HOME")
-            else:
+                    destination = os.environ.get("HOME")
+                else:
                 # Folder specified? Use that one.
-                destination = parts[1].strip()
+                    destination = parts[1].strip()
             # If the user types "cd /Desktop", this grabs "/Desktop"
-            if destination and destination.startswith("~"):
-                home = os.environ.get("HOME")
+                if destination and destination.startswith("~"):
+                    home = os.environ.get("HOME")
                 # This swaps the ~ for the actual home path (like /home/user)
-                destination = destination.replace("~", home, 1)
+                    destination = destination.replace("~", home, 1)
             #
             # How .replace() works:
             # # Use commas to separate arguments: 1st is what to find ("~"),
             # 2nd is what to replace it with (home),
             # 3rd is "1" to only replace the first occurrence.
             #
-        try:
-            if destination:
-                os.chdir(destination)
+            try:
+                if destination:
+                    os.chdir(destination)
             # chdir stands for CHange DIRectory
         # Catches error if folder doesn't exist
-        except FileNotFoundError:
-            print(f"cd: {destination}: No such file or directory")
-        except NotADirectoryError:
-            print(f"cd: {destination}: Not a directory")
+            except FileNotFoundError:
+                print(f"cd: {destination}: No such file or directory")
+            except NotADirectoryError:
+                print(f"cd: {destination}: Not a directory")
         return
 
 
